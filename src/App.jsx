27a759d3e594468816dac5c82fd5a4b0dd5e56b4
PauRaset <94690926+PauRaset@users.noreply.github.com>
2026-04-03 12:00:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from 'react'
+
 export default function App() {
   return (
-    <div>
-      <h1>Hola</h1>
-      <p>La web funciona</p>
+    <div style={{ background: 'red', minHeight: '100vh', color: 'white', fontSize: '60px' }}>
+      HOLA
     </div>
   )
 }
